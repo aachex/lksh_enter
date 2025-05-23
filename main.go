0d88d0668c11406fc9053c895178cbd61d8ba4b6
artemwebber1 <chekhonin.artem@gmail.com>
2025-05-23 21:42:09 +0300

    changed name fetching

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"slices"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -33,13 +35,62 @@ type match struct {
 
 var client http.Client
 
+var teamId map[string]int
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		panic(err)
 	}
 
-	// TODO: precount teams
+	var p player
+	players := []player{}
+	playerNames := []string{}
+	id := 1
+
+loop:
+	for {
+		req, err := http.NewRequest(http.MethodGet, os.Getenv("API_HOST")+fmt.Sprintf("/players/%d", id), nil)
+		if err != nil {
+			panic(err)
+		}
+		req.Header.Set("Authorization", os.Getenv("API_TOKEN"))
+
+		res, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		switch res.StatusCode {
+		case http.StatusTooManyRequests:
+			res.Body.Close()
+			time.Sleep(time.Minute)
+			continue
+		case http.StatusNotFound:
+			break loop
+		}
+
+		b, err := io.ReadAll(res.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		err = json.Unmarshal(b, &p)
+		if err != nil {
+			panic(err)
+		}
+
+		players = append(players, p)
+		playerNames = append(playerNames, p.Name+" "+p.Surname)
+
+		fmt.Println(id)
+		res.Body.Close()
+		id++
+	}
+
+	slices.Sort(playerNames)
+	for _, n := range playerNames {
+		fmt.Println(n)
+	}
 
 	var s string
 	for {
@@ -54,9 +105,6 @@ func main() {
 func getStats() {
 	var teamName string
 	fmt.Scan(&teamName)
-
-	var teamId int
-	// teamId = ...
 
 	req, err := http.NewRequest(http.MethodGet, os.Getenv("API_HOST")+"/matches", nil)
 	if err != nil {
@@ -78,11 +126,5 @@ func getStats() {
 	err = json.Unmarshal(b, &matches)
 	if err != nil {
 		panic(err)
-	}
-
-	for _, m := range matches {
-		if m.Team1Id != teamId && m.Team2Id != teamId {
-			continue
-		}
 	}
 }
