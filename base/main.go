29af503392a314bef69a64a081718f479af6e95a
artemwebber1 <chekhonin.artem@gmail.com>
2025-05-25 12:15:06 +0300

    created package general

--- base/main.go
+++ base/main.go
@@ -10,31 +10,9 @@ import (
 	"slices"
 	"time"
 
+	"github.com/aachex/lksh_enter/general"
 	"github.com/joho/godotenv"
 )
-
-type player struct {
-	Id      int    `json:"id"`
-	Name    string `json:"name"`
-	Surname string `json:"surname"`
-	Number  int    `json:"number"`
-}
-
-type team struct {
-	Id      int    `json:"id"`
-	Name    string `json:"name"`
-	Players []int  `json:"players"`
-}
-
-type match struct {
-	Id         int `json:"id"`
-	Team1Id    int `json:"team1"`
-	Team2Id    int `json:"team2"`
-	Team1Score int `json:"team1_score"`
-	Team2Score int `json:"team2_score"`
-}
-
-var client http.Client
 
 func main() {
 	err := godotenv.Load("../.env")
@@ -42,13 +20,15 @@ func main() {
 		panic(err)
 	}
 
-	var p player
+	client := http.Client{}
+
+	var p general.Player
 	playerNames := []string{}
 	id := 1
 
 loop:
 	for {
-		req := getRequest(os.Getenv("API_HOST") + fmt.Sprintf("/players/%d", id))
+		req := general.GetRequest(os.Getenv("API_HOST") + fmt.Sprintf("/players/%d", id))
 
 		res, err := client.Do(req)
 		if err != nil {
@@ -85,14 +65,14 @@ loop:
 	}
 
 	// fetch teams
-	teams := mustFetch[[]team](os.Getenv("API_HOST") + "/teams")
+	teams := general.MustFetch[[]general.Team](os.Getenv("API_HOST")+"/teams", &client)
 	teamId := make(map[string]int)
 	for _, t := range teams {
 		teamId[t.Name] = t.Id
 	}
 
 	// fetch matches
-	matches := mustFetch[[]match](os.Getenv("API_HOST") + "/matches")
+	matches := general.MustFetch[[]general.Match](os.Getenv("API_HOST")+"/matches", &client)
 
 	var s string
 	in := bufio.NewReader(os.Stdin)
@@ -105,12 +85,12 @@ loop:
 				panic(err)
 			}
 			teamName = teamName[1 : len(teamName)-3]
-			getStats(teamName, teamId, matches)
+			general.GetStats(teamName, teamId, matches)
 
 		case "versus?":
 			var id1, id2 int
 			fmt.Scan(&id1, &id2)
-			versus(id1, id2, teams, matches)
+			general.Versus(id1, id2, teams, matches)
 		}
 	}
 }
