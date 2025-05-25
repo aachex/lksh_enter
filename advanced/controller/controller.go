577e6d94bd5dc12dfefc3816ac7f542ff531858b
artemwebber1 <chekhonin.artem@gmail.com>
2025-05-25 14:10:09 +0300

    edits

--- advanced/controller/controller.go
+++ advanced/controller/controller.go
@@ -1,9 +1,27 @@
 package controller
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strings"
 
-type Controller struct{}
+	"github.com/aachex/lksh_enter/general"
+)
+
+type Controller struct {
+	Matches []general.Match
+	TeamId  map[string]int
+	Teams   []general.Team
+	Client  http.Client
+}
+
+func (c *Controller) RegisterEndpoints(mux *http.ServeMux) {
+	mux.HandleFunc("GET /stats", c.GetStats)
+}
 
 func (c *Controller) GetStats(w http.ResponseWriter, r *http.Request) {
-
+	teamName := strings.Trim(r.URL.Query().Get("team_name"), "\"")
+	teamId := c.TeamId[teamName]
+	wins, defeats, diff := general.GetStats(teamId, c.Matches)
+	w.Write(fmt.Appendf(nil, "%d %d %d", wins, defeats, diff))
 }
--- advanced/main.go
+++ advanced/main.go
@@ -1,10 +1,47 @@
 package main
 
-import "github.com/joho/godotenv"
+import (
+	"net/http"
+	"os"
+
+	"github.com/aachex/lksh_enter/advanced/controller"
+	"github.com/aachex/lksh_enter/general"
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	err := godotenv.Load("../.env")
 	if err != nil {
 		panic(err)
 	}
+
+	client := http.Client{}
+
+	// fetch teams
+	teams := general.MustFetch[[]general.Team](os.Getenv("API_HOST")+"/teams", &client)
+	teamId := make(map[string]int) // get team id by name
+	for _, t := range teams {
+		teamId[t.Name] = t.Id
+	}
+
+	// fetch matches
+	matches := general.MustFetch[[]general.Match](os.Getenv("API_HOST")+"/matches", &client)
+
+	mux := http.NewServeMux()
+
+	controller := controller.Controller{
+		Teams:   teams,
+		TeamId:  teamId,
+		Matches: matches,
+		Client:  client,
+	}
+
+	controller.RegisterEndpoints(mux)
+
+	srv := http.Server{
+		Handler: mux,
+		Addr:    ":" + os.Getenv("SRV_PORT"),
+	}
+
+	srv.ListenAndServe()
 }
